4c031ba4037c9e11eb9e954faebece068bc7d911
Berardino Chiarello <berardino95@gmail.com>
2023-12-05 09:31:57 +0100

    Indentation changes

--- Bullseye/Views/PointsView.swift
+++ Bullseye/Views/PointsView.swift
@@ -8,43 +8,43 @@
 import SwiftUI
 
 struct PointsView: View {
-  
-  @Binding var alertIsShowed: Bool
-  @Binding var sliderValue: Double
-  @Binding var game: Game
-  
-  var body: some View {
-    let roundedValue = Int(sliderValue.rounded())
-    let points = game.calculatePoint(sliderValue: roundedValue)
     
-    VStack(spacing: 15){
-      InstructionsText(text: "The slider'e value is")
-      BigNumberText(target: "\(roundedValue)")
-      BodyText(text: "You scored \(points) Points\n 🎉🎉🎉")
-      
-      Button(action: {
-        withAnimation {
-          game.startNewRound(points: game.calculatePoint(sliderValue: Int(sliderValue)))
-          alertIsShowed = false
+    @Binding var alertIsShowed: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        let roundedValue = Int(sliderValue.rounded())
+        let points = game.calculatePoint(sliderValue: roundedValue)
+        
+        VStack(spacing: 15){
+            InstructionsText(text: "The slider'e value is")
+            BigNumberText(target: "\(roundedValue)")
+            BodyText(text: "You scored \(points) Points\n 🎉🎉🎉")
+            
+            Button(action: {
+                withAnimation {
+                    game.startNewRound(points: game.calculatePoint(sliderValue: Int(sliderValue)))
+                    alertIsShowed = false
+                }
+            }, label: {
+                ButtonText(text: "Start New Round")
+            })
         }
-      }, label: {
-        ButtonText(text: "Start New Round")
-      })
+        .padding()
+        .frame(maxWidth: 300)
+        .background( Color.background )
+        .clipShape(.rect(cornerRadius: Constants.General.roundedRectCornerRadius, style: .continuous))
+        .shadow(radius: 10, x: 5)
     }
-    .padding()
-    .frame(maxWidth: 300)
-    .background( Color.background )
-    .clipShape(.rect(cornerRadius: Constants.General.roundedRectCornerRadius, style: .continuous))
-    .shadow(radius: 10, x: 5)
-  }
 }
 
 #Preview {
-  PointsView(alertIsShowed: .constant(true), sliderValue: .constant(50), game: .constant(Game()))
+    PointsView(alertIsShowed: .constant(true), sliderValue: .constant(50), game: .constant(Game()))
 }
 
 #Preview {
-  PointsView(alertIsShowed: .constant(true), sliderValue: .constant(50), game: .constant(Game()))
-    .preferredColorScheme(.dark)
-    .previewInterfaceOrientation(.landscapeLeft)
+    PointsView(alertIsShowed: .constant(true), sliderValue: .constant(50), game: .constant(Game()))
+        .preferredColorScheme(.dark)
+        .previewInterfaceOrientation(.landscapeLeft)
 }
--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -2,74 +2,74 @@
 import SwiftUI
 
 struct RoundedImageViewStroked: View {
-  var symbol: String
-  var body: some View {
-    Image(systemName: symbol)
-      .font(.title)
-      .padding(20)
-      .foregroundStyle(.text)
-      .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
-      .overlay {
-        Circle()
-          .strokeBorder(.roundedButtonStroke, lineWidth: Constants.General.strokeWidth)
-      }
-  }
+    var symbol: String
+    var body: some View {
+        Image(systemName: symbol)
+            .font(.title)
+            .padding(20)
+            .foregroundStyle(.text)
+            .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
+            .overlay {
+                Circle()
+                    .strokeBorder(.roundedButtonStroke, lineWidth: Constants.General.strokeWidth)
+            }
+    }
 }
 
 struct RoundedImageViewFilled: View {
-  var symbol: String
-  var body: some View {
-    Image(systemName: symbol)
-      .font(.title)
-      .padding(20)
-      .foregroundStyle(.circleFilledButtonText)
-      .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
-      .background(.circleFilledButtonBackground)
-      .clipShape(Circle())
-  }
+    var symbol: String
+    var body: some View {
+        Image(systemName: symbol)
+            .font(.title)
+            .padding(20)
+            .foregroundStyle(.circleFilledButtonText)
+            .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
+            .background(.circleFilledButtonBackground)
+            .clipShape(Circle())
+    }
 }
 
 struct RoundedRectTextView:View {
-  var text: String
-  var body: some View {
-    Text(text)
-      .font(.title2)
-      .kerning(-0.2)
-      .bold()
-      .frame(width: Constants.General.roundedViewWidth, height: Constants.General.roundedViewHeight)
-      .overlay {
-        RoundedRectangle(cornerRadius: Constants.General.roundedRectCornerRadius, style: .continuous)
-          .strokeBorder(.roundedButtonStroke ,lineWidth: Constants.General.strokeWidth)
-      }
-  }
+    var text: String
+    var body: some View {
+        Text(text)
+            .font(.title2)
+            .kerning(-0.2)
+            .bold()
+            .frame(width: Constants.General.roundedViewWidth, height: Constants.General.roundedViewHeight)
+            .overlay {
+                RoundedRectangle(cornerRadius: Constants.General.roundedRectCornerRadius, style: .continuous)
+                    .strokeBorder(.roundedButtonStroke ,lineWidth: Constants.General.strokeWidth)
+            }
+    }
 }
 
 struct PreviewView: View {
-  var body: some View {
-    VStack(spacing: 50){
-      HStack(spacing: 50){
-        RoundedImageViewStroked(symbol: "arrow.counterclockwise")
-        RoundedImageViewFilled(symbol: "arrow.counterclockwise")
-      }
-      
-      HStack(spacing: 50){
-        RoundedImageViewStroked(symbol: "list.bullet")
-        RoundedImageViewFilled(symbol: "list.bullet")
-      }
-      
-      HStack(spacing: 50){
-        RoundedRectTextView(text: "999")
-        RoundedRectTextView(text: "12")
-      }
+    var body: some View {
+        VStack(spacing: 50){
+            HStack(spacing: 50){
+                RoundedImageViewStroked(symbol: "arrow.counterclockwise")
+                RoundedImageViewFilled(symbol: "arrow.counterclockwise")
+            }
+            
+            HStack(spacing: 50){
+                RoundedImageViewStroked(symbol: "list.bullet")
+                RoundedImageViewFilled(symbol: "list.bullet")
+            }
+            
+            HStack(spacing: 50){
+                RoundedRectTextView(text: "999")
+                RoundedRectTextView(text: "12")
+            }
+        }
     }
-  }
 }
 
 #Preview {
-  PreviewView()
+    PreviewView()
 }
 
 #Preview {
-  PreviewView()
-    .preferredColorScheme(.dark)
+    PreviewView()
+        .preferredColorScheme(.dark)
 }
